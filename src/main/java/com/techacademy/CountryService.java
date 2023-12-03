49f190300fbd62503b0c3a50d1f6ae58fa8bc117
itakehiko <takehiko.iwasaki.24@gmail.com>
2023-12-03 18:48:25 +0900

    Country更新・削除

--- src/main/java/com/techacademy/CountryService.java
+++ src/main/java/com/techacademy/CountryService.java
@@ -1,9 +1,11 @@
 package com.techacademy;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CountryService {
@@ -17,4 +19,23 @@ public class CountryService {
     public List<Country> getCountryList() {
         return repository.findAll();
     }
+
+    public Country getCountry(String code) {
+        Optional<Country> option = repository.findById(code);
+        Country country = option.orElse(null);
+
+        return country;
+    }
+
+    @Transactional
+    public void updateCountry(String code, String name, int population) {
+        Country country = new Country(code, name, population);
+        repository.save(country);
+    }
+
+    @Transactional
+    public void deleteCountry(String code) {
+        repository.deleteById(code);
+    }
+
 }
--- src/main/java/com/techacademy/CountyController.java
+++ src/main/java/com/techacademy/CountyController.java
@@ -2,8 +2,7 @@ package com.techacademy;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("country")
@@ -19,5 +18,36 @@ public class CountyController {
         model.addAttribute("countrylist", service.getCountryList());
 
         return "country/list";
+    }
+
+    @GetMapping(value = { "/detail", "/detail/{code}/"})
+    public String getCountry(@PathVariable(name = "code", required = false) String code, Model model) {
+        Country country = code != null ? service.getCountry(code) : new Country();
+        model.addAttribute("country", country);
+
+        return "country/detail";
+    }
+
+    @PostMapping("/detail")
+    public String postCountry(
+            @RequestParam("code") String code,
+            @RequestParam("name") String name,
+            @RequestParam("population") int population,
+            Model model) {
+        service.updateCountry(code, name, population);
+
+        return "redirect:/country/list";
+    }
+
+    @GetMapping("/delete")
+    public String deleteCountryForm(Model model) {
+        return "country/delete";
+    }
+
+    @PostMapping("/delete")
+    public String deleteCounty(@RequestParam("code") String code, Model model) {
+        service.deleteCountry(code);
+
+        return "redirect:/country/list";
     }
 }
