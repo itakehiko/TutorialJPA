9de35d55376aa0c8abb939f173af375851802feb
itakehiko <takehiko.iwasaki.24@gmail.com>
2023-12-03 18:17:32 +0900

    Country一覧

---
+++ src/main/java/com/techacademy/CountryService.java
@@ -0,0 +1,20 @@
+package com.techacademy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+    private final CountryRepository repository;
+
+    @Autowired
+    public CountryService(CountryRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Country> getCountryList() {
+        return repository.findAll();
+    }
+}
---
+++ src/main/java/com/techacademy/CountyController.java
@@ -0,0 +1,23 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("country")
+public class CountyController {
+    private final CountryService service;
+
+    public CountyController(CountryService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/list")
+    public String getList(Model model) {
+        model.addAttribute("countrylist", service.getCountryList());
+
+        return "country/list";
+    }
+}
